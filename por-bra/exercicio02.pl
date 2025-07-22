7f2884837ff89f882bd4293e5629a41042f321a5
Tiago Fe <31408208+gumdanx@users.noreply.github.com>
2025-07-22 14:16:21 -0300

    Update exercicio02.pl
    Add final explanation about `once` command

--- por-bra/exercicio02.pl
+++ por-bra/exercicio02.pl
@@ -21,5 +21,8 @@ amigos(X, Y) :- gosta(X, Z), gosta(Y, Z), X \= Y.
 inimigos(X, Y) :- (tem_pet(X, cachorro), tem_pet(Y, gato));
     			        (tem_pet(X, gato), tem_pet(Y, cachorro)).
 amizade_forte(X, Y) :- once((gosta(X, K1), gosta(Y, K1), 
-							               gosta(X, K2), gosta(Y, K2),
-							               K1 \= K2)).
+                             gosta(X, K2), gosta(Y, K2),
+                             K1 \= K2)).
+% Comando `once` é útil, mas encerra o backtracking no primeiro resultado
+% Se você perguntar quem tem amizade forte com maria `amizade_forte(Quem, maria)`
+% você não verá os demais casos, use o `once` com cuidado
