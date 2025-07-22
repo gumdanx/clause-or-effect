5eb551fd6f5bf02571b1a87fbc856ecad417d8c5
Tiago Fe <31408208+gumdanx@users.noreply.github.com>
2025-07-22 13:36:26 -0300

    Create exercicio01.pl
    Exercise 01 from a Logic Programming course

---
+++ por-bra/exercicio01.pl
@@ -0,0 +1,36 @@
+% Exercicio #01 Fatos e Consultas:
+%
+% Fatos:
+% Pessoas e comidas que elas gostam
+gosta(maria, chocolate).
+gosta(joao, futebol).
+gosta(ana, pizza).
+gosta(ana, chocolate).
+gosta(joao, sushi).
+gosta(maria, salada).
+gosta(carla, pizza).
+gosta(joao, pizza).
+%
+% Pessoas e seus pets
+tem_pet(ana, cachorro).
+tem_pet(joao, gato).
+tem_pet(maria, peixe).
+tem_pet(carla, papagaio).
+%
+% Consultas: 
+?- gosta(ana, pizza).
+% true
+
+?- gosta(joao, lasanha).
+% false
+
+?- tem_pet(maria, peixe).
+% true
+
+?- tem_pet(joao, cachorro).
+% false
+
+?- gosta(Quem, pizza).
+% Quem = ana ;
+% Quem = carla ;
+% Quem = joao.
